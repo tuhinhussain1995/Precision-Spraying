2b8b85fa8328a571a52f966f39176b3a653405b1
Md Tuhin Hossain <tuhin.hossain@hashcodestudio.com>
2024-04-23 23:39:33 +0800

    simple update in video

---
+++ yolo_v8/3.0 live_cam_predict/run_directly/video/live_video.py
@@ -0,0 +1,34 @@
+from ultralytics import YOLO
+import cv2
+
+model = YOLO("best.pt")
+
+# Open the video file
+video_path = "C:/Users/tuhin/Desktop/Precision-Spraying/yolo_v8/3.0 live_cam_predict/run_directly/video/video.mp4"
+cap = cv2.VideoCapture(video_path)
+
+# Loop through the video frames
+while cap.isOpened():
+    # Read a frame from the video
+    success, frame = cap.read()
+
+    if success:
+        # Run YOLOv8 inference on the frame
+        results = model(frame)
+
+        # Visualize the results on the frame
+        annotated_frame = results[0].plot()
+
+        # Display the annotated frame
+        cv2.imshow("YOLOv8 Inference", annotated_frame)
+
+        # Break the loop if 'q' is pressed
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    else:
+        # Break the loop if the end of the video is reached
+        break
+
+# Release the video capture object and close the display window
+cap.release()
+cv2.destroyAllWindows()
