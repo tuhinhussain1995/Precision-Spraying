368272da83c19d63b581e3249959165ddcb21ff3
Md Tuhin Hossain <tuhin.hossain@hashcodestudio.com>
2024-04-25 13:03:29 +0800

    camera done

--- yolo_v8/3.0 live_cam_predict/angular_app/src/app/live-stream/live-stream.component.ts
+++ yolo_v8/3.0 live_cam_predict/angular_app/src/app/live-stream/live-stream.component.ts
@@ -1,29 +1,32 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-live-stream',
   templateUrl: './live-stream.component.html',
   styleUrls: ['./live-stream.component.scss']
 })
-export class LiveStreamComponent {
+export class LiveStreamComponent implements OnDestroy {
   @ViewChild('video') video!: ElementRef<HTMLVideoElement>;
   @ViewChild('canvas') canvas!: ElementRef<HTMLCanvasElement>;
   videoElement!: HTMLVideoElement;
+  stream!: MediaStream | null;
 
-  constructor() {
-
-  }
+  constructor() {}
 
   ngAfterViewInit() {
     this.videoElement = this.video.nativeElement;
   }
 
-  startProcess(){
+  ngOnDestroy() {
+    this.stopCamera();
+  }
+
+  startProcess() {
     this.startCamera();
 
-    setTimeout(() =>{
+    setTimeout(() => {
       this.takePicture();
-    },1000);
+    }, 1000);
   }
 
   startCamera() {
@@ -33,22 +36,31 @@ export class LiveStreamComponent {
       })
       .then(stream => {
         this.videoElement.srcObject = stream;
+        this.stream = stream;
       })
       .catch(error => {
         console.error('Error accessing webcam:', error);
       });
   }
 
+  stopCamera() {
+    if (this.stream) {
+      this.stream.getTracks().forEach(track => {
+        track.stop();
+      });
+    }
+  }
+
   takePicture() {
     const canvasElement = this.canvas.nativeElement;
     const context = canvasElement.getContext('2d');
-  
+
     if (this.videoElement && context) {
       canvasElement.width = this.videoElement.videoWidth;
       canvasElement.height = this.videoElement.videoHeight;
-  
+
       context.drawImage(this.videoElement, 0, 0, canvasElement.width, canvasElement.height);
-  
+
       // Convert the canvas content to a base64 string
       const base64ImageData = canvasElement.toDataURL('image/png').split(',')[1]; // Extract base64 string after comma
       console.log('Base64 Image:', base64ImageData);
@@ -56,5 +68,4 @@ export class LiveStreamComponent {
       console.error('Video element or canvas context is not initialized.');
     }
   }
-  
 }
